3fcea0224f07755a807823daabecadeaa40c1d50
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-08 11:33:35 +0100

    Updated quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,13 +1,21 @@
 print("Welcome to my computer quiz !")
-playing = input("Do you want to play? ")
+playing = input("Do you want to play? ").lower()
 if playing != "yes":
     quit()
 
 print("Okay! Let's play :)")
 
-answer = input("What does CPU stand for? ")
+questions = {
+    "What does CPU stand for? ": "central processing unit",
+    "What does GPU stand for? ": "graphics processing unit",
+    "What does RAM stand for? ": "random access memory",
+    "What does PSU stand for? ": "power supply"
+}
 
-if answer == "central processing unit": 
-    print("Correct!")
-else: 
-    print("Incorrect!")
+for question, correct_answer in questions.items():
+    answer = input(question)
+    if answer.lower() == correct_answer:
+        print("Correct!")
+    else:
+        print("Incorrect!")
+
