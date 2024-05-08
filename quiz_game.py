5619c921c2516a1f05be8e96f1bfccccdcfc9e31
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-08 12:11:55 +0100

    Updated quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -10,7 +10,10 @@ questions = {
     "What does CPU stand for? ": "central processing unit",
     "What does GPU stand for? ": "graphics processing unit",
     "What does RAM stand for? ": "random access memory",
-    "What does PSU stand for? ": "power supply"
+    "What does PSU stand for? ": "power supply",
+    "What does HTML stand for? ": "hypertext markup language",
+    "What does PDF stand for? ": "portable document format",
+    "What does HTTP stand for? ": "hypertext transfer protocol"
 }
 
 for question, correct_answer in questions.items():
@@ -20,6 +23,19 @@ for question, correct_answer in questions.items():
         score += 1
     else:
         print("Incorrect!")
+
+hardQuestions = {
+    "What does BIOS stand for? ": "basic input/output system",
+    "What does USB stand for? ": "universal serial bus",
+}
+
+for question, correct_answer in hardQuestions.items():
+    answer = input(question)
+    if answer.lower() == correct_answer:
+        print("Correct!")
+        score += 2
+    else:
+        print("Incorrect!")
  
-print("You got " + str(score) + " question correct!")
-print("You got " + str(score/4 * 100) + "%.")
+print("You got " + str(score) + "/11!")
+print("You got " + str(score/9 * 100) + "%.")
